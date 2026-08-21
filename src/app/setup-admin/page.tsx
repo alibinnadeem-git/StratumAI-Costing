ace1334a11a7be7c9f1837aa3621b0d0755c7153
alibinnadeem-git <alibinnadeem.official@gmail.com>
2026-08-20 22:54:26 -0700

    Force admin bootstrap page to render at request time (#9)

--- src/app/setup-admin/page.tsx
+++ src/app/setup-admin/page.tsx
@@ -2,6 +2,8 @@ import bcrypt from "bcryptjs";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function SetupAdminPage() {
   const existingUsers = await db.user.count();
   if (existingUsers > 0) redirect("/login");
